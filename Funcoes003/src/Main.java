bc9d2f2e6e80acf5322aef4acdc8b6bee9e2badf
Ricardo Fontana <fontanaricardo@gmail.com>
2016-06-21 16:17:12 -0300

    Adiciondo exercícios funções 3

---
+++ Funcoes003/src/Main.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		/*
+		 * Crie duas funções, uma que retorno o menor dos três números,
+		 * outra que retorne o maior do três números
+		 */
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("Digite três números: ");
+		
+		int n1 = scanner.nextInt();
+		int n2 = scanner.nextInt();
+		int n3 = scanner.nextInt();
+
+	}
+
+}
